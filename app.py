83df31042e73996d7d3fca9b244a62da1979b077
SalmanAd01 <salmanadhikari415@gmail.com>
2023-03-24 10:51:05 +0530

    added the linter

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
-from flask import Flask,request,jsonify 
 import pickle
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
-lod_pickle = pickle.load(open('./model/anemia.pkl', 'rb'))
+with open("./model/anemia.pkl", "rb") as f:
+    lod_pickle = pickle.load(f)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-@app.route('/predict')
+
+@app.route("/predict")
 def predict():
     json = request.get_json()
-    gender = json['Gender']
-    hemoglobin = json['Hemoglobin']
-    mch = json['MCH']
-    mchc = json['MCHC']
-    mcv = json['MCV']
-    to_predict = [
-        gender,
-        hemoglobin,
-        mch,
-        mchc,
-        mcv
-    ]
+    gender = json["Gender"]
+    hemoglobin = json["Hemoglobin"]
+    mch = json["MCH"]
+    mchc = json["MCHC"]
+    mcv = json["MCV"]
+    to_predict = [gender, hemoglobin, mch, mchc, mcv]
     to_predict = [to_predict]
     prediction = lod_pickle.predict(to_predict)
 
-    return jsonify(
-        prediction = prediction.tolist()[0]
-    )
+    return jsonify(prediction=prediction.tolist()[0])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
